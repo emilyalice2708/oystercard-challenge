9be631752b0dd7591a4dd9d70f589acc0afaef90
Dan Holmes <dh@danielholmes.com>
2020-10-09 15:34:45 +0100

    create journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,2 +1,15 @@
 class Journey
+  attr_reader :entry_station, :exit_station
+
+  def start(station)
+    @entry_station = station
+  end
+
+  def end(station)
+    @exit_station = station
+  end
+
+  def fare
+    1
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,29 +2,21 @@ require "journey"
 
 describe Journey do
   let(:entry_station) { double(:station) }
+  let(:exit_station) { double(:station) }
   let(:oystercard) { double :oystercard }
+  let(:journey) { Journey.new }
 
-  before do
-    allow(oystercard).to receive(:touch_in).and_return(entry_station)
+  it "store the entry station" do
+    journey.start(entry_station)
+    expect(journey.entry_station).to eq entry_station
   end
 
-  # describe "#start_journey" do
-  #   subject.start_journey(:entry_station)
-  #   expect(subject.entry_station).to eq entry_station
-  # end
-
-  # it "can store a entry station" do
-  #   oystercard.touch_in(entry_station)
-  #
-  #   expect(subject).to eq()
-  # end
-
-  it "can store an exit station" do
+  it "stores the exit station" do
+    journey.end(exit_station)
+    expect(journey.exit_station).to eq exit_station
   end
 
-  it "returns the minimum fare if journey completed correctly" do
-  end
-
-  it "returns the penalty fare if journey is incomplete" do
+  it "returns a far of 1" do
+    expect(journey.fare).to eq 1
   end
 end
